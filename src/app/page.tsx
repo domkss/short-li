ecbb647fd8a2e718c1b64964d72cd93210706477
Dominik Kiss <kissdominik7@gmail.com>
2023-11-29 23:41:09 +0100

    Add main layout for URL input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,14 @@ import LongURLInput from "@/components/LongURLInput";
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between'>
-      <LongURLInput />
+    <main className='min-h-screen flex flex-col items-center'>
+      {/*Main URL input layout*/}
+      <div className='absolute top-1/3 container max-w-2xl'>
+        <div className='p-4  shadow-md bg-gray-400 bg-opacity-20 border-[1px] border-solid sm:rounded-md border-gray-100'>
+          <h3 className='font-medium text-xl ms-3'>Short any long URL for Free</h3>
+          <LongURLInput />
+        </div>
+      </div>
     </main>
   );
 }
--- src/components/LongURLInput.tsx
+++ src/components/LongURLInput.tsx
@@ -1,23 +1,55 @@
+"use client";
+import { useState } from "react";
+import clsx from "clsx";
+
+enum ProgressState {
+  NotStarted,
+  Loading,
+  Finished,
+}
+
 export default function LongURLInput() {
+  const [shortedURL, setShortedURL] = useState("");
+  const [progressStatus, setProgressStatus] = useState<ProgressState>(0);
+
   return (
     <div className='p-4'>
-      <form>
-        <div className='relative shadow-xl'>
+      <div>
+        <div className='relative shadow-md rounded-lg'>
           <input
             type='url'
             id='url-input'
-            className='pr-[110px] block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 ps-5 text-sm text-gray-900 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500'
+            className='pr-[125px] block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 ps-5 text-sm
+             text-gray-900 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500'
             placeholder='example.com/long?url'
-            required
+            disabled={progressStatus == ProgressState.Loading}
           />
           <button
             type='submit'
-            className='absolute bottom-2.5 end-2.5 rounded-lg bg-blue-600 px-4 py-2 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+            onClick={() => {
+              setProgressStatus(ProgressState.Loading);
+              setShortedURL("Dummy");
+            }}
+            className={clsx(
+              "absolute bottom-2.5 end-2.5 rounded-lg bg-blue-600 px-4 py-2 text-sm font-medium text-white",
+              progressStatus == ProgressState.Loading
+                ? ""
+                : "hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-300 flex items-center"
+            )}
+            disabled={progressStatus == ProgressState.Loading}
           >
-            Short it
+            <div className={clsx("inline-block mr-2")}>Short it</div>
+            <div
+              className={clsx(
+                "inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current",
+                "border-r-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite] border-white",
+                progressStatus !== ProgressState.Loading ? "hidden" : ""
+              )}
+              role='status'
+            />
           </button>
         </div>
-      </form>
+      </div>
     </div>
   );
 }
