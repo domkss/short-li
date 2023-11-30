d0180dba0d870f241c9f661b88f95c00b0799d6d
Dominik Kiss <kissdominik7@gmail.com>
2023-11-30 22:01:03 +0100

    Improve main input UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import LongURLInput from "@/components/LongURLInput";
 
 export default function Home() {
   return (
-    <main className='min-h-screen flex flex-col items-center'>
+    <main className='min-h-screen flex flex-col items-center bg-emerald-100'>
       {/*Main URL input layout*/}
       <div className='absolute top-1/3 container max-w-2xl'>
-        <div className='p-4  shadow-md bg-gray-400 bg-opacity-20 border-[1px] border-solid sm:rounded-md border-gray-100'>
-          <h3 className='font-medium text-xl ms-3'>Short any long URL for Free</h3>
+        <div className='p-4 shadow-md bg-gray-400 bg-opacity-20 sm:rounded-md'>
+          <h3 className='font-medium text-xl text-center'>Short any long URL for Free</h3>
           <LongURLInput />
         </div>
       </div>
--- src/components/LongURLInput.tsx
+++ src/components/LongURLInput.tsx
@@ -12,43 +12,70 @@ export default function LongURLInput() {
   const [shortedURL, setShortedURL] = useState("");
   const [progressStatus, setProgressStatus] = useState<ProgressState>(0);
 
+  function onSubmitClick() {
+    setProgressStatus(ProgressState.Loading);
+    setShortedURL("Dummy");
+  }
+
   return (
     <div className='p-4'>
-      <div>
-        <div className='relative shadow-md rounded-lg'>
-          <input
-            type='url'
-            id='url-input'
-            className='pr-[125px] block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 ps-5 text-sm
-             text-gray-900 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500'
-            placeholder='example.com/long?url'
-            disabled={progressStatus == ProgressState.Loading}
-          />
+      <div className='relative'>
+        <input
+          type='url'
+          id='url-input'
+          className='md:pr-[125px] block w-full rounded-lg border border-gray-300 bg-gray-50 p-5 ps-5 text-sm
+             text-gray-900 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 shadow-md'
+          placeholder='example.com/long?url'
+          disabled={progressStatus == ProgressState.Loading}
+        />
+        {/*Inline submit button for large screens*/}
+        <div className='max-md:hidden'>
           <button
             type='submit'
-            onClick={() => {
-              setProgressStatus(ProgressState.Loading);
-              setShortedURL("Dummy");
-            }}
+            onClick={() => onSubmitClick()}
             className={clsx(
-              "absolute bottom-2.5 end-2.5 rounded-lg bg-blue-600 px-4 py-2 text-sm font-medium text-white",
+              "absolute bottom-2.5 end-2.5 rounded-lg bg-blue-600 px-4 py-2 text-md text-white",
               progressStatus == ProgressState.Loading
                 ? ""
                 : "hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-300 flex items-center"
             )}
             disabled={progressStatus == ProgressState.Loading}
           >
-            <div className={clsx("inline-block mr-2")}>Short it</div>
+            <div className={clsx("inline-block mr-2 text-xl")}>Short it</div>
             <div
               className={clsx(
-                "inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current",
-                "border-r-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite] border-white",
+                "inline-block h-5 w-5 animate-spin rounded-full border-2 border-solid border-current",
+                "border-r-transparent align-[-0.175em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite] border-white",
                 progressStatus !== ProgressState.Loading ? "hidden" : ""
               )}
               role='status'
             />
           </button>
         </div>
+      </div>
+      {/*Submit button for mobiles*/}
+      <div className='md:hidden mt-3 flex items-center flex-col'>
+        <button
+          type='submit'
+          onClick={() => onSubmitClick()}
+          className={clsx(
+            "rounded-lg bg-blue-600 py-2 text-sm text-white min-w-[50%]",
+            progressStatus == ProgressState.Loading
+              ? ""
+              : "hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-300"
+          )}
+          disabled={progressStatus == ProgressState.Loading}
+        >
+          <div className={clsx("inline-block mr-2 text-xl")}>Short it</div>
+          <div
+            className={clsx(
+              "inline-block h-5 w-5 animate-spin rounded-full border-2 border-solid border-current",
+              "border-r-transparent align-[-0.175em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite] border-white",
+              progressStatus !== ProgressState.Loading ? "hidden" : ""
+            )}
+            role='status'
+          />
+        </button>
       </div>
     </div>
   );
