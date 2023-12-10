6a6c931ce6df97fb7c345ab2b3544aaf0e34ba36
Dominik Kiss <kissdominik7@gmail.com>
2023-12-10 22:50:15 +0100

    Navbar Mobile UX fix, main page UI update, rename redis folder to docker

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -105,6 +105,9 @@ function NavBar() {
                   ? "bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
                   : "text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
               }
+              onClick={() => {
+                setMobileMenuOpened(!mobileMenuOpened);
+              }}
               aria-current='page'
             >
               {item.title}
