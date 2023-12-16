875e4e11d26f47136ece8d70fd952256b4a89d5d
Dominik Kiss <kissdominik7@gmail.com>
2023-12-16 14:12:58 +0100

    Update database structure

---
+++ src/app/user/dashboard/_devConsts.ts
@@ -0,0 +1,12 @@
+export const DummyURLs = [
+  { name: "ExampleSite1", url: "https://www.examplesite1" },
+  { name: "SampleSite2", url: "https://www.samplesite2.n" },
+  { name: "WebPortal3", url: "https://www.webportal3.org" },
+  { name: "GlobalHub4", url: "https://www.globalhub4.inf" },
+  { name: "VirtualWorld5", url: "https://www.virtualworl" },
+  { name: "TechInnovate6", url: "https://www.techinnovat" },
+  { name: "NexusNetwork7", url: "https://www.nexusnetwor" },
+  { name: "DataSphere8", url: "https://www.datasphere8.d" },
+  { name: "QuantumLink9", url: "https://www.quantumlink9" },
+  { name: "CyberSpace10", url: "https://www.cyberspace10" },
+];
--- src/components/session-world-map/SessionMap.tsx
+++ src/components/session-world-map/SessionMap.tsx
@@ -6,31 +6,40 @@ export default function SessionMap() {
   let zoomx = 2000;
   let zoomy = zoomx * 0.4285;
   return (
-    <div className=''>
-      <svg
-        className='inline-block'
-        baseProfile='tiny'
-        fill='#ececec'
-        height='500'
-        stroke='black'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-        stroke-width='.2'
-        version='1.2'
-        viewBox={`${2000 / 2 - zoomx / 2} ${857 / 2 - zoomy / 2} ${zoomx} ${zoomy}`}
-        width='750'
-        xmlns='http://www.w3.org/2000/svg'
-      >
-        {WorldSVGData.map((item, key) => (
-          <path
-            className='stroke-white stroke-[0.5]  hover:stroke-black hover:stroke-1 hover:cursor-pointer fill-gray-300'
-            key={key}
-            name={item.name}
-            id={item.id}
-            d={item.d}
-          />
-        ))}
-      </svg>
+    <div className='p-3 border-2 mt-5 border-slate-300 shadow-md rounded-md max-sm:min-w-[350px] min-w-[50%]'>
+      <div>
+        <svg
+          width='auto'
+          height='auto'
+          baseProfile='tiny'
+          fill='#ececec'
+          stroke='black'
+          stroke-linecap='round'
+          stroke-linejoin='round'
+          stroke-width='.2'
+          version='1.2'
+          viewBox={`${2000 / 2 - zoomx / 2} ${857 / 2 - zoomy / 2} ${zoomx} ${zoomy}`}
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          {WorldSVGData.map((item, key) => (
+            <path
+              className='stroke-white stroke-[0.5]  hover:stroke-gray-800 hover:stroke-2 hover:cursor-pointer fill-gray-300'
+              key={key}
+              name={item.name}
+              id={item.id}
+              d={item.d}
+            />
+          ))}
+        </svg>
+      </div>
+      <div className='flex flex-row'>
+        <div className='flex flex-col mx-1 w-9 h-9 border-[1px]  border-gray-400 rounded-full justify-center align-middle text-center'>
+          <span className='text-xl font-extrabold'>-</span>
+        </div>
+        <div className='flex flex-col mx-1 w-9 h-9 border-[1px]  border-gray-400 rounded-full justify-center align-middle text-center'>
+          <span className='text-lg font-bold'>+</span>
+        </div>
+      </div>
     </div>
   );
 }
