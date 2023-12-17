459c2c6df667a898b4c600845d939aa206705a72
Dominik Kiss <kissdominik7@gmail.com>
2023-12-18 00:30:06 +0100

    User links page side bar updates

--- src/app/user/links/page.tsx
+++ src/app/user/links/page.tsx
@@ -12,16 +12,24 @@ export default function Dashboard() {
   //if (session.data?.user) {
   return (
     <div className='flex flex-row max-sm:flex-col'>
-      <div className='basis-1/3 mt-2'>
+      <div className='basis-1/3 min-w-0'>
+        <div className='flex flex-row border-b-2 border-blue-200 text-center p-3 bg-indigo-100 0 shadow-md rounded-b-lg'>
+          <div className='flex basis-1/3 items-center justify-center'>
+            <Image className='' src='/links_undraw.svg' width={38} height={38} alt='My Links icon' />
+          </div>
+          <div className='flex bais-2/3'>
+            <span className='text-xl font-semibold'>My Links</span>
+          </div>
+        </div>
         <ul>
           {DummyURLs.map((item, key) => (
             <li key={key}>
-              <div className='border-b-[1px] border-slate-200 cursor-pointer hover:bg-slate-100'>
-                <div className='flex flex-row  flex-wrap items-center'>
+              <div className='border-b-[1px] border-slate-200 cursor-pointer hover:bg-slate-100 shadow-sm'>
+                <div className='flex flex-row flex-nowrap items-center'>
                   <span className='ml-4'>{key + "."}</span>
-                  <div className='flex flex-col p-3'>
-                    <span className='mx-2 font-semibold'>{item.name}</span>
-                    <span className='mx-2'>{item.url}</span>
+                  <div className='flex flex-col overflow-ellipsis overflow-hidden whitespace-nowrap'>
+                    <span className='px-2 font-semibold'>{item.name}</span>
+                    <span className='px-2'>{item.shortURL}</span>
                   </div>
                 </div>
               </div>
@@ -30,8 +38,8 @@ export default function Dashboard() {
         </ul>
       </div>
       <div className='flex flex-col basis-2/3 p-2 '>
-        <div className='flex flex-row p-3 '>
-          Name{" "}
+        <div className='flex flex-row p-3'>
+          <span className='mx-2 font-semibold text-2xl text-gray-800 font-serif'>Google Calendar Link</span>
           <Image className='mx-2 cursor-pointer' src='/edit_pencil.svg' width={20} height={20} alt='Edit pencil icon' />
         </div>
 
