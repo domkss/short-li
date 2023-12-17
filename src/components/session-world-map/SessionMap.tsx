f303a6249f88eaf9d411499ba2fbefc5f5b28030
Dominik Kiss <kissdominik7@gmail.com>
2023-12-17 15:56:57 +0100

    Add mouse wheel event listener

--- src/components/session-world-map/SessionMap.tsx
+++ src/components/session-world-map/SessionMap.tsx
@@ -2,6 +2,7 @@
 
 import { WorldSVGData } from "./world-svg-data";
 import clsx from "clsx";
+import { useEffect, createRef } from "react";
 
 export default function SessionMap() {
   let zoomx = 2000;
@@ -15,19 +16,37 @@ export default function SessionMap() {
     "fill-blue-200",
     "fill-blue-300",
   ];
+  const mapContainer = createRef<HTMLDivElement>();
+
+  const handleZoom = () => {};
+
+  useEffect(() => {
+    const container = mapContainer.current;
+    const handleWheel = (event: any) => {
+      event.preventDefault();
+      const { deltaY } = event;
+      let scrollDirection = deltaY > 0 ? -1 : 1;
+      console.log(scrollDirection);
+    };
+
+    if (container) {
+      container.addEventListener("wheel", handleWheel);
+      return () => {
+        container.removeEventListener("wheel", handleWheel);
+      };
+    }
+  }, [mapContainer]);
 
   return (
     <div className='p-3 border-2 mt-5 border-slate-300 shadow-md rounded-md max-sm:min-w-[350px] min-w-[50%]'>
-      <div>
+      <div ref={mapContainer}>
         <svg
-          width='auto'
-          height='auto'
           baseProfile='tiny'
           fill='#ececec'
           stroke='black'
-          stroke-linecap='round'
-          stroke-linejoin='round'
-          stroke-width='.2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          strokeWidth='.2'
           version='1.2'
           viewBox={`${2000 / 2 - zoomx / 2} ${857 / 2 - zoomy / 2} ${zoomx} ${zoomy}`}
           xmlns='http://www.w3.org/2000/svg'
@@ -46,6 +65,7 @@ export default function SessionMap() {
           ))}
         </svg>
       </div>
+
       <div className='flex flex-row'>
         <div className='flex flex-col mx-1 w-9 h-9 border-[1px]  border-gray-400 rounded-full justify-center align-middle text-center'>
           <span className='text-xl font-extrabold'>-</span>
