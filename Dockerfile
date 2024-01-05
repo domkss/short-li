9c81b49018322c3a1aa3d8615015d013035a37dd
Dominik Kiss <kissdominik7@gmail.com>
2024-01-05 17:20:33 +0100

    Add bugfixes found in production

--- Dockerfile
+++ Dockerfile
@@ -54,7 +54,7 @@ RUN chown nextjs:nodejs .next
 COPY --from=builder --chown=nextjs:nodejs /app/.next/standalone ./
 COPY --from=builder --chown=nextjs:nodejs /app/.next/static ./.next/static
 COPY --from=builder --chown=nextjs:nodejs /app/.env.production ./
-COPY --from=builder --chown=nextjs:nodejs /app/resources ./
+COPY --from=builder --chown=nextjs:nodejs /app/resources ./resources
 
 USER nextjs
 
--- src/app/[url]/route.ts
+++ src/app/[url]/route.ts
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const ip = (req.headers.get("x-forwarded-for") || "").split(",").pop()?.trim();
+  const ip = (req.headers.get("x-forwarded-for") || "").split(",").shift()?.trim();
   let shortURL = req.nextUrl.pathname.substring(1);
   if (shortURL) {
     let destinationURL = await getDestinationURL(shortURL, ip);
