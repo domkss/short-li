4f4c439d75e48d4f757adffab6445ce6a2a6d133
Dominik Kiss <kissdominik7@gmail.com>
2024-02-15 18:16:22 +0100

    Start implement the link in bio page config page

--- src/app/user/links/bio/page.tsx
+++ src/app/user/links/bio/page.tsx
@@ -11,10 +11,22 @@ export default function CustomBioDashboard() {
     reactRouter.replace("/login");
 
   return (
-    <div className="flex min-h-screen min-w-full items-center">
-      <div className="flex min-w-full flex-col items-center">
-        <span className="mb-4 text-lg">Under development. Check back later.</span>
-        <Image src="/icons/indevelopment_undraw.svg" width={240} height={240} alt="" />
+    <div className="flex min-w-full flex-col">
+      <div className="flex flex-col p-4">
+        <div className="flex flex-col items-center">
+          <button className="rounded-xl bg-purple-500 p-4 text-white">sli.ink/bioLink</button>
+        </div>
+        <div className="mb-6 max-w-xl">
+          <label htmlFor="large-input" className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+            Description
+          </label>
+          <input
+            type="text"
+            id="large-input"
+            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 text-base text-gray-900 focus:border-blue-500 
+            focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          />
+        </div>
       </div>
     </div>
   );
