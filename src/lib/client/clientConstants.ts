f543b35157da60f75ab582802c5568f0af9d6b95
Dominik Kiss <kissdominik7@gmail.com>
2024-02-21 19:27:24 +0100

    Remove Link-in-bio page from nav bar

--- src/lib/client/clientConstants.ts
+++ src/lib/client/clientConstants.ts
@@ -1,5 +1,4 @@
 export const navBarLinks = [
   { title: "URL Shortener", path: "/", authRequired: false },
   { title: "My Links", path: "/user/links", authRequired: true },
-  { title: "Link-in-bio", path: "/user/links/bio", authRequired: true },
 ];
