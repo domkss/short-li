e5d16886e8747b39b8fa265edf17fc8da356678a
Dominik Kiss <kissdominik7@gmail.com>
2024-03-20 23:03:53 +0100

    Add orderable list to custom bio page designer view

--- src/app/user/links/bio/page.tsx
+++ src/app/user/links/bio/page.tsx
@@ -2,14 +2,16 @@
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import SocialMediaRefBar from "@/components/atomic/SocialMediaRefBar";
+import OrderableListLayout from "@/components/atomic/OrderableListLayout";
 
 export default function CustomBioDashboard() {
+  /*
   const reactRouter = useRouter();
   const session = useSession();
   let isServer = typeof window === "undefined" ? true : false;
   if (!isServer && (session.status !== "authenticated" || !session.data || !session.data.user))
     reactRouter.replace("/login");
-
+*/
   return (
     <main className="flex min-w-full flex-col">
       <div className="flex flex-col p-4">
@@ -20,10 +22,25 @@ export default function CustomBioDashboard() {
             convallis urna volutpat. Sed ullamcorper elit id enim lacinia, at feugiat lorem efficitur.
           </div>
         </div>
-        <div className="my-2 flex justify-center">
-          <button className="basis-full rounded-md bg-cyan-200 px-5  py-3 md:basis-2/3 xl:basis-1/3">Twitter</button>
+        <div className="flex justify-center">
+          <OrderableListLayout className="flex basis-full flex-col md:basis-2/3 xl:basis-1/3">
+            <div className="mt-2 rounded-md border border-gray-300 bg-cyan-200 px-5 py-3 text-center shadow-sm" id="1">
+              Twitter
+            </div>
+
+            <div className="mt-2 rounded-md border border-gray-300 bg-blue-200 px-5 py-3 text-center shadow-sm" id="2">
+              Facebook
+            </div>
+          </OrderableListLayout>
         </div>
-        <div className="my-2 mt-12 flex justify-center">
+
+        <div className="flex justify-center py-2">
+          <button className="basis-full rounded-md border border-gray-300 px-5 py-3 shadow-sm md:basis-2/3 xl:basis-1/3">
+            + Add
+          </button>
+        </div>
+
+        <div className="mt-12 flex justify-center">
           <SocialMediaRefBar />
         </div>
       </div>
---
+++ src/components/atomic/OrderableListLayout.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState, ReactElement } from "react";
+import { DragDropContext, Droppable, Draggable, DroppableProps } from "react-beautiful-dnd";
+import { cn } from "@/lib/client/uiHelperFunctions";
+interface Props {
+  children: KeyedReactElement[];
+  className?: string;
+}
+
+type KeyedReactElement = React.ReactElement<{ id: string; className?: string }>;
+
+const OrderableListLayout: React.FC<Props> = ({ children, className }) => {
+  const [items, setItems] = useState<KeyedReactElement[]>(children);
+
+  const onDragEnd = (result: any) => {
+    if (!result.destination) return;
+
+    const reorderedItems = React.Children.toArray(items) as KeyedReactElement[];
+    const [reorderedItem] = reorderedItems.splice(result.source.index, 1);
+    reorderedItems.splice(result.destination.index, 0, reorderedItem);
+
+    setItems(reorderedItems);
+  };
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <StrictModeDroppable droppableId="reorder-layout">
+        {(provided: any) => (
+          <div {...provided.droppableProps} ref={provided.innerRef} className={className}>
+            {items.map((item, index) => (
+              <Draggable key={item.props.id} draggableId={item.props.id} index={index}>
+                {(provided: any) => (
+                  <>
+                    {React.cloneElement(item, {
+                      ref: provided.innerRef,
+                      ...provided.draggableProps,
+                      ...provided.dragHandleProps,
+                      className: item.props.className,
+                    })}
+                  </>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </StrictModeDroppable>
+    </DragDropContext>
+  );
+};
+
+const StrictModeDroppable = ({ children, ...props }: DroppableProps) => {
+  const [enabled, setEnabled] = useState(false);
+
+  useEffect(() => {
+    const animation = requestAnimationFrame(() => setEnabled(true));
+
+    return () => {
+      cancelAnimationFrame(animation);
+      setEnabled(false);
+    };
+  }, []);
+
+  if (!enabled) {
+    return null;
+  }
+
+  return <Droppable {...props}>{children}</Droppable>;
+};
+
+export default OrderableListLayout;
