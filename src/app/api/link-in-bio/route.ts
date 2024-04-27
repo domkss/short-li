18317d2843ed4212b5b75d482a4188dfacfef7d9
Dominik Kiss <kissdominik7@gmail.com>
2024-04-27 18:29:59 +0200

    Add api calls and route handlers to get and patch button list on bio page

--- src/app/api/link-in-bio/route.ts
+++ src/app/api/link-in-bio/route.ts
@@ -5,12 +5,13 @@ import {
   getCurrentUserLinkInBioPageId,
   getLinkInBioDescription,
   getLinkInBioLinkButtons,
+  setLinkInBioLinkButtons,
 } from "@/lib/server/api-functions";
 import { getServerSession } from "next-auth";
 import authOptions from "../auth/[...nextauth]/authOptions";
 import { formatShortLink } from "@/lib/server/serverHelperFunctions";
 import { isSessionWithEmail } from "@/lib/client/dataValidations";
-
+import { linkInBioButtonItemsSchema } from "@/lib/client/dataValidations";
 //Get User Links
 export async function GET(req: NextRequest) {
   const session = await getServerSession(authOptions);
@@ -37,4 +38,20 @@ export async function GET(req: NextRequest) {
     description: description,
     link_buttons: linkInBioLinkButtons,
   });
+}
+
+export async function PATCH(req: NextRequest) {
+  const content = await req.json();
+
+  const parsedContent = linkInBioButtonItemsSchema.safeParse(content);
+  if (!parsedContent.success) return Response.json({ success: false }, { status: HTTPStatusCode.BAD_REQUEST });
+
+  const session = await getServerSession(authOptions);
+
+  if (isSessionWithEmail(session)) {
+    let result = await setLinkInBioLinkButtons(parsedContent.data, session);
+    return Response.json({ success: result });
+  } else {
+    return Response.json({ success: false }, { status: HTTPStatusCode.UNAUTHORIZED });
+  }
 }
