67e4b6a41791b515d335237eab25c09a04aa90ab
Dominik Kiss <kissdominik7@gmail.com>
2024-05-12 12:01:20 +0200

    Move the suspense up to the layout from the return statement

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,5 +1,19 @@
 import ReCaptchaProvider from "@/components/providers/ReCaptchaProvider";
+import { Suspense } from "react";
+import LoadingSpinner from "@/components/atomic/LoadingSpinner";
 
 export default function LoginLayout({ children }: { children: React.ReactNode }) {
-  return <ReCaptchaProvider>{children}</ReCaptchaProvider>;
+  return (
+    <ReCaptchaProvider>
+      <Suspense
+        fallback={
+          <div className="my-auto">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        {children}
+      </Suspense>
+    </ReCaptchaProvider>
+  );
 }
