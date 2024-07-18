74ef8487d161f943503a04e263c8cde93ac74255
Dominik Kiss <kissdominik7@gmail.com>
2024-07-18 22:45:30 +0200

    Fix a bug causing URL-s with a lot of querry params resulting in a redirect error

--- src/app/[url]/route.ts
+++ src/app/[url]/route.ts
@@ -8,9 +8,26 @@ export async function GET(req: NextRequest) {
 
   let shortURL = req.nextUrl.pathname.substring(1);
   if (shortURL) {
-    let destinationURL = await getDestinationURL(shortURL, ip);
+    let destinationURL = encodeQueryParams(await getDestinationURL(shortURL, ip));
     redirect(destinationURL);
   } else {
     redirect("/");
   }
+}
+
+function encodeQueryParams(url: string) {
+  const [base, queryString] = url.split("?");
+  if (!queryString) {
+    return url; // No query string to encode
+  }
+
+  const encodedParams = queryString
+    .split("&")
+    .map((param) => {
+      const [key, value] = param.split("=");
+      return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+    })
+    .join("&");
+
+  return `${base}?${encodedParams}`;
 }
