f6ead3c88bfdf128cf160d9b1253ae8297c5c518
Dominik Kiss <kissdominik7@gmail.com>
2024-10-31 17:18:25 +0100

    Remove param encoding druing redirect

--- src/app/[url]/route.ts
+++ src/app/[url]/route.ts
@@ -8,27 +8,9 @@ export async function GET(req: NextRequest) {
 
   let shortURL = req.nextUrl.pathname.substring(1);
   if (shortURL) {
-    let destinationURL = encodeQueryParams(await getDestinationURL(shortURL, ip));
+    let destinationURL = await getDestinationURL(shortURL, ip);
     redirect(destinationURL);
   } else {
     redirect("/");
   }
-}
-
-function encodeQueryParams(url: string) {
-  const [base, queryString] = url.split("?");
-  if (!queryString) {
-    return url; // No query string to encode
-  }
-
-  const encodedParams = queryString
-    .split(/[&;]/) // Split by either '&' or ';'
-    .map((param) => {
-      const [key, ...valueParts] = param.split("=");
-      const value = valueParts.join("=");
-      return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
-    })
-    .join("&");
-
-  return `${base}?${encodedParams}`;
 }
